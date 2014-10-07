cdf7ae2cabce57b650b6b9d8274434a12bbd8edb
Brent Schultz <bschultz@degdigital.com>
2014-10-07 15:00:31 -0500

    initial commit

---
+++ DegImageHelper.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: bschultz
+ * Date: 8/21/14
+ * Time: 4:38 PM
+ *
+ *
+ */
+
+/**
+ * OPTIONS AND POST ASSUME USE OF ADVANCED CUSTOM FIELDS
+ **/
+
+class DegImageHelperFactory{
+    public static function createFromOptions($fieldName, $size){
+        return new DegImageHelper($fieldName, 'options', $size);
+    }
+    public static function createFromPost($fieldName, $size){
+        return new DegImageHelper($fieldName, 'post', $size);
+    }
+    public static function createFromID($id, $size){
+        return new DegImageHelper(false, false, $size, $id);
+    }
+}
+class DegImageHelper
+{
+    public $src;
+    public $alt;
+    public $imgTag;
+    public $type;
+    public $size;
+    public $fieldName;
+    public $postID;
+    public $imgID;
+
+    public function __construct($fieldName = false, $type = false, $size = "full", $id = false){
+
+        $this->type = $type;
+        $this->size = $size;
+        $this->fieldName = $fieldName;
+        $id ? $this->imgID = $id : $this->imgID = $this->getID($this->fieldName, $this->type);
+        $this->src = $this->getSrc($this->size);
+        $this->alt = $this->getAlt();
+
+    }
+    public function getSrc(){
+        $src = wp_get_attachment_image_src($this->imgID, $this->size);
+        $src = $src[0];
+        return $src;
+    }
+    public function getID($fieldName, $type){
+        if($type === 'post'){
+            $imgID = get_field($fieldName);
+        }else{
+            $imgID = get_field($fieldName, 'options');
+        }
+        return $imgID;
+    }
+    public function getAlt(){
+        $alt = get_post_meta($this->imgID, '_wp_attachment_image_alt', true);
+        return $alt;
+    }
+    public function buildImgTag($classes){
+        $tag = '<img src="'.$this->src.'" class="'.$classes.'">';
+        return $tag;
+    }
+
+}
